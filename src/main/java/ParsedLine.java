76d11a697dcab1464613e2c99bb8bb511cb63abc
Андрей <a.yahin@tematelecom.ru>
2025-07-02 17:18:20 +0300

    Обработка большого файла

--- src/main/java/ParsedLine.java
+++
@@ -1,9 +0,0 @@
-public class ParsedLine {
-    final String original;
-    final String[] parts;
-
-    ParsedLine(String original, String[] parts) {
-        this.original = original;
-        this.parts = parts;
-    }
-}
